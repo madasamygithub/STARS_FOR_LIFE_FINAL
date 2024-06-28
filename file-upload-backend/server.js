2cce2b430acdaa1ebaaf31e168ac19057d5add19
Madasamy S <128239231+madasamygithub@users.noreply.github.com>
2024-06-29 00:01:22 +0530

    Update server.js

--- file-upload-backend/server.js
+++ file-upload-backend/server.js
@@ -29,11 +29,12 @@ app.use(express.urlencoded({ extended: true }));
 
 const MONGO_URI = process.env.MONGO_URI;
 
-console.log(MONGO_URI, {
+mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
-
+})
+.then(() => console.log('MongoDB connected successfully'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 
 const storage = multer.diskStorage({
